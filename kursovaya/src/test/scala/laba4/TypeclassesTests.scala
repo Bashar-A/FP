4dc29c7345ba75d109af2bb6b3178e57a5957bc9
Bashar Almamma <basharmamma@hotmail.com>
2020-06-13 15:29:07 +0300

    Added tests for laba4

--- kursovaya/src/test/scala/laba4/TypeclassesTests.scala
+++ kursovaya/src/test/scala/laba4/TypeclassesTests.scala
@@ -3,5 +3,31 @@ package laba4
 import org.scalatest.FunSuite
 
 class TypeclassesTests extends FunSuite{
-    
+    test("ReversableString") {
+        val string = "abc"
+        val res = "cba"
+        val reversableString = Typeclasses.testReversableString(string)
+        assert(reversableString == res)
+    }
+    test("SmashInt") {
+        val a = 3
+        val b = 4
+        val res = 7
+        val smashInt = Typeclasses.testSmashInt(a, b)
+        assert(smashInt == res)
+    }
+    test("SmashDouble") {
+        val a = 3
+        val b = 4
+        val res = 12
+        val smashDouble = Typeclasses.testSmashDouble(a, b)
+        assert(smashDouble == res)
+    }
+    test("SmashString") {
+        val a = "ab"
+        val b = "cd"
+        val res = "abcd"
+        val smashString = Typeclasses.testSmashString(a, b)
+        assert(smashString == res)
+    }
 }
